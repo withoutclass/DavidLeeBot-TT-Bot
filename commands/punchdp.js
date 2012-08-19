a347c310a7582d0f2a48d7cc91522578148f33fb
Ronald Bruintjes <ronaldb64@gmail.com>
2012-08-19 09:05:31 -0400

    Add /punchdp command DJ Danger Princess is a DJ in BO80s who loves to punch people. This is revenge...

---
+++ commands/punchdp.js
@@ -0,0 +1,19 @@
+exports.name = '/punchdp';
+exports.hidden = true;
+exports.enabled = true;
+exports.matchStart = false;
+exports.handler = function(data) {
+    if (admincheck(data.userid)) {
+        // 4e132721a3f75114de033e4b is DJ Danger Princess
+        // Is DP in room?
+        var DPisHere = false;
+        for (i in usersList) {
+            if (usersList[i].id = '4e132721a3f75114de033e4b') {
+                DPisHere = true;
+            }
+        }
+        if (DPisHere) {
+            bot.speak('/me punches DP');
+        }
+    }
+}
